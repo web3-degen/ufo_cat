8fa88a93f00cb270e455df98febc87ce5faecf57
git-marcopitra <marcomorete73@hotmail.com>
2024-04-09 17:22:41 +0100

    :fire: chore: remove background

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,4 +1,4 @@
-import { Div, Main } from '@stylin.js/elements';
+import { Main } from '@stylin.js/elements';
 import { FC, PropsWithChildren } from 'react';
 
 import SEO from '../seo';
@@ -6,13 +6,6 @@ import SEO from '../seo';
 const Layout: FC<PropsWithChildren> = ({ children }) => (
   <>
     <SEO />
-    <Div
-      inset="0"
-      position="absolute"
-      backgroundImage="linear-gradient(#fff8, #fff8), url('/img/bg.webp')"
-      backgroundPosition="center center"
-      backgroundSize="cover"
-    />
     <Main
       display="flex"
       minHeight="100vh"
